b12eac615b4a0364126704e2943b51fc958841c0
flamekaiser007 <arpitmaurya262005@gmail.com>
2026-07-16 14:35:30 +0530

    leetcode: solved "Find Mode in Binary Search Tree"

---
+++ leetcode/find-mode-in-binary-search-tree/solution.cpp
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+     int currVal = 0;
+        int currCount = 0;
+        int maxCount = 0;
+
+void inorder(TreeNode* root)
+{
+    if(root == nullptr)
+return ;
+
+inorder(root->left);
+
+if(root->val == currVal)
+currCount++;
+else{
+    currVal = root->val;
+    currCount = 1;
+}
+if(currCount >maxCount){
+    maxCount = currCount;
+    modes.clear();
+    modes.push_back(currVal);
+}
+else if(currCount == maxCount)
+{
+     modes.push_back(currVal);
+    }
+inorder(root->right);
+}
+    vector<int> findMode(TreeNode* root) {
+   inorder(root);
+   return modes;
+        
+    }
+};
